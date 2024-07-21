f164314956f3eb383a614cfefbc11b1715c8e4d4
Alexander Lavrov <devnickflamel@gmail.com>
2024-07-21 16:54:02 +0300

    complete sign up page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,15 +1,52 @@
-import React from 'react'
+import { useState } from 'react'
 import {Link} from  'react-router-dom'
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.id]: e.target.
+      value });
+  }
+
+  /*const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch('/api/auth/signup', formData);
+    const data = await res.json();
+    console.log(data);
+  };*/
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData)
+      })
+      const data = await res.json();
+      console.log(data);
+      setLoading (false);
+      if (data.success === false ){
+        setError(true);
+        return;
+      }
+    } catch (error) {
+      setLoading (false);
+      setError(true);
+    }
+  };
   return (
     <div className='p-3 max-w-lg mx-auto'>
         <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-        <form className='flex flex-col gap-4'>
-          <input type="text" placeholder="Username" id='username' className='bg-slate-100 p-3 rounded-lg'/>
-          <input type="text" placeholder="Email" id='email' className='bg-slate-100 p-3 rounded-lg'/>
-          <input type="password" placeholder="Password" id='password' className='bg-slate-100 p-3 rounded-lg'/>
-          <button type="submit" className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign Up</button>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+          <input type="text" placeholder="Username" id='username' className='bg-slate-100 p-3 rounded-lg' onChange={handleChange}/>
+          <input type="text" placeholder="Email" id='email' className='bg-slate-100 p-3 rounded-lg' onChange={handleChange} />
+          <input type="password" placeholder="Password" id='password' className='bg-slate-100 p-3 rounded-lg' onChange={handleChange}/>
+          <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{ loading ? 'Loading...' : 'Sign Up'}</button>
         </form>
         <div className="flex gap-2 mt-5">
           <p>Already have an account?</p>
@@ -17,6 +54,7 @@ export default function SignUp() {
           <span className='text-blue-500'>Sign in</span>
           </Link>
         </div>
+        <p className='text-red-700'>{error && 'Something went wrong!'} </p>
     </div>
-  )
+  );
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  //plugins: [react()],
+  server: {
+    proxy:{
+      '/api': {
+        target: 'http://192.168.13.146:3000',
+        secure: false,  
+      } ,
+    },
+  },
+  plugins: [react()],  
+});
