ef22541cfe394a4ad3fa0455a689ea9112c219fd
Alexander Lavrov <devnickflamel@gmail.com>
2024-07-21 13:31:34 +0300

    create sign up api

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,20 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req,res) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save()
+    res.status(201).json({ message: "User registered successfully!" });
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+
+
+
+
+
+
+};
