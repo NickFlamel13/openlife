df3064e3f6dbc5644ee9736b8d542501211dac3a
Alexander Lavrov <devnickflamel@gmail.com>
2024-07-23 20:35:21 +0300

    Change error code

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -19,9 +19,9 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const validUser = await User.findOne({ email });
-    if (!validUser) return next(errorHandler(401, "User not found"));
+    if (!validUser) return next(errorHandler(404, "User not found"));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
-    if (!validPassword) return next(errorHandler(401, "wrong credentials"));
+    if (!validPassword) return next(errorHandler(404, "wrong credentials"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const {password: hashedPassword, ...rest } = validUser._doc;
     const expiryDate = new Date(Date.now() + 3600000); // 1 hour expiry
