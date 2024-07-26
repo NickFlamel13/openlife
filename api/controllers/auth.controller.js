1785efbe90bfa6ccb6dfe13123f9e8fe1fb79bee
Alexander Lavrov <devnickflamel@gmail.com>
2024-07-26 20:42:29 +0300

    added sign out user func

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -78,3 +78,10 @@ export const google = async (req, res, next) => {
     next(error);
   }
 };
+
+
+export const signout = (req, res) => {
+  res.clearCookie('access_token') .status(200).json('User signed out successfully!');
+};
+
+
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -15,6 +15,7 @@ import {
   deleteUserStart,
   deleteUserSuccess,
   deleteUserFailure,
+  signOut,
 } from "../redux/user/userSlice";
 
 export default function Profile() {
@@ -96,7 +97,17 @@ export default function Profile() {
       dispatch(deleteUserFailure(error));
 
     }
-  }
+  };
+  const handleSignOut = async () => {
+    try {
+      await fetch('/api/auth/signout');
+      dispatch(signOut());
+    } catch (error) {
+      console.log(error);
+    }
+
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -156,7 +167,7 @@ export default function Profile() {
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteAccount} className="text-red-700 cursor-pointer">Delete Account</span>
-        <span className="text-red-700 cursor-pointer">Sign Out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
       <p className="text-red-700 mt-5">{error && 'Something went wrong!'}</p>
       <p className="text-green-700 mt-5">{updateSuccess && 'User is updated successfully!'}</p>
